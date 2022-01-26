66e5dd5085ebc022426949479aafd4f8e40295d5
Guillermo <nuriellez@gmail.com>
2022-01-26 13:59:17 +0100

    Avance con el prefab del zombie

--- My project/Assets/Scripts/ZombieMove.cs
+++ My project/Assets/Scripts/ZombieMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ZombiMove : MonoBehaviour
+public class ZombieMove : MonoBehaviour
 {
     //Variables para el NaveMeshAgent
     float zombiSpeed;
